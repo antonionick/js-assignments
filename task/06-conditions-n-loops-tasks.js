e9fb8cdb4d89da6489b19af2d8808b19ec28be2c
Anton Golovko <golovko639@gmail.com>
2019-06-07 13:57:09 +0300

    feat: create rectangles overlap task

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -134,7 +134,12 @@ function isTriangle(a, b, c) {
  *
  */
 function doRectanglesOverlap(rect1, rect2) {
-	throw new Error('Not implemented');
+	if (rect1.top + rect1.height < rect2.top || rect2.top + rect2.height < rect1.top) {
+		return false;
+	} else if (rect1.left + rect1.width < rect2.left || rect2.left + rect2.width < rect1.left) {
+		return false;
+	}
+	return true;
 }
 
 
